ae38420f86e430176db795a2cfa2038dcc00df92
katrinesando <kmsa@itu.dk>
2022-11-16 22:19:06 +0100

    prints hello there 3x times

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,7 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println("Hello there");
+        System.out.println("Hello there");
     }
 }
