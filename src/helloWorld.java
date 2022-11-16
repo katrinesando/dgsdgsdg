24231a2d93b69a5aa132acd7b680d3b914657849
katrinesando <kmsa@itu.dk>
2022-11-16 22:23:43 +0100

    made new calc functions

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,5 +3,12 @@ public class helloWorld {
         System.out.println("Hello there");
         System.out.println("Hello there");
         System.out.println("Hello there");
+        calc(3);
+    }
+    public static int calc(int num){
+        if (num%2==0){
+            return 0;
+        }
+        return 1;
     }
 }
